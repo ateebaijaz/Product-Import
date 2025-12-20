36cc8a8e0ee39d75b62fdb4a774e94d08df31c65
Ateeb Mir <ateeb.mir@kredily.com>
2025-12-20 13:14:54 +0530

    fix: conditional SSL for Render postgre

--- config/settings.py
+++ config/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+    "default": dj_database_url.config(
         conn_max_age=600,
+        ssl_require=bool(os.getenv("RENDER")),
     )
 }
 
