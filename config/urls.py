dd978205152d1d262818554f3239347ad0c6e19e
Ateeb Mir <ateeb.mir@kredily.com>
2025-12-20 13:26:57 +0530

    fix: redirect root to product page

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
+from django.shortcuts import redirect
 
+
+def root_redirect(request):
+    return redirect("/products/")
 
 urlpatterns = [
+    path("", root_redirect),
     path('admin/', admin.site.urls),
     path('imports/', include('imports.urls')),
     path('products/', include('products.urls')),
