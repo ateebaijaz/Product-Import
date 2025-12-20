a4e7b97f8cfec07b481316bd4eaa40b51a139de1
Ateeb Mir <ateeb.mir@kredily.com>
2025-12-20 10:50:51 +0530

    minor filters and soft deletion in ui

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import product_list, delete_all_products
+from .views import product_list, delete_all_products, toggle_product_active
 
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('delete-all/', delete_all_products, name='delete_all_products'),
+    path('toggle/<int:product_id>/', toggle_product_active, name='toggle_product_active'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -4,11 +4,21 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.views.decorators.http import require_POST
 
+from django.shortcuts import get_object_or_404
+
 
 def product_list(request):
     products = Product.objects.all().order_by('sku')
-    paginator = Paginator(products, 50)  # 50 per page
+    sku = request.GET.get('sku')
+    is_active = request.GET.get('is_active')
 
+    if sku:
+        products = products.filter(sku__icontains=sku)
+
+    if is_active in ['true', 'false']:
+        products = products.filter(is_active=(is_active == 'true'))
+
+    paginator = Paginator(products, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -18,10 +28,20 @@ def product_list(request):
         {
             "products": page_obj,
             "page_obj": page_obj,
+            "sku": sku or "",
+            "is_active": is_active or "",
         }
     )
 
 @require_POST
 def delete_all_products(request):
     Product.objects.all().delete()
+    return redirect("/products/")
+
+
+@require_POST
+def toggle_product_active(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.is_active = not product.is_active
+    product.save(update_fields=["is_active"])
     return redirect("/products/")
